4a30c9cc5fb41c05ef33544c56b9e20a1c85a001
sidhdhali <sidhnakrani1911@gmail.com>
2024-05-13 13:02:27 +0200

    make form and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import Form from "./Form";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <>
+  <Form />  
+  <div className="bg-gray-300 p-10">
+    <div className="flex flex-row justify-center ">
+    <h1 className="text-4xl font-bold text-wrap w-40 font-serif m-4">My Log Travels</h1>
+    <div className="flex flex-col justify-center items-start">
+        <h2 className="font-bold justify-items-start  m-4">Quick Links</h2>
+        <div className="flex flex-row">
+
+        <ul className="text-start mr-6 ">
+          <li className="p-2 text-sm ">Home</li>
+          <li className="p-2 text-sm ">About Us</li>
+          <li className="p-2 text-sm ">My TravelLogs</li>
+        </ul>
+      
+
+   
+      <ul className="text-start mr-6">
+        <li className="p-2 text-sm ">Travels</li>
+        <li className="p-2 text-sm " >PromoFlights</li>
+        <li className="p-2 text-sm ">Travel Routes</li>
+      </ul>
+    
+
+    
+      <ul className="text-start mr-6">
+        <li className="p-2 text-sm ">Recommendations</li>
+        <li className="p-2 text-sm ">Guides</li>
+        <li className="p-2 text-sm ">Blogs</li>
+      </ul>
+    
+  
+    
+    <ul className="text-start ">
+      <li className="p-2 text-sm ">Mission Statement</li>
+      <li className="p-2 text-sm ">The Team</li>
+      <li className="p-2 text-sm ">Contact Us</li>
+    </ul>
+  
+  </div>
+    </div>
+
+</div>
+    <p className="text-center p-4 m-6" >2024-Group-4 My Log Travels.ALL Rights Reserved.</p>
+    
+    </div>
+    </>;
 };
 
 export default Footer;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import form_image  from '../assets/form_image.avif'; 
+
 
 const Form = () => {
-  return <div>Form</div>;
+  return <div className="flex flext-col justify-center m-10">
+    
+    <div className="flex flex-col items-center justify-center w-screen ">
+      <h1 className="text-5xl text-center font-bold m-4 w-80 font-serif">Be updated!</h1>
+      
+      <form className="flex flex-col">
+      <input placeholder="First Name*" className="border-2 border-black rounded w-80 placeholder-black text-sm m-2 p-2" />
+      <input placeholder="Last Name*"  className="border-2 border-black rounded w-80  text-black text-sm m-2 p-2 placeholder-black"/>
+      <input placeholder="Email Subject*"  className="border-2 border-black rounded w-80  text-black text-sm m-2 p-2 placeholder-black"/>
+      <textarea placeholder="Write something here..."  className="border-2 border-black rounded w-80  text-black text-sm h-40 m-2 p-2 placeholder-black" />
+      <button className="bg-black text-white w-80 border rounded p-2 m-4">Submit Email</button>
+      </form>
+    </div>
+
+    <div className=" w-screen  ">
+      <img src={form_image}  alt="a travelling see photo"/>
+    </div>
+  
+  
+  </div>;
 };
 
 export default Form;
